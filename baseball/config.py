e16118fdba1b89bd076756e4b22af3534ce3ab0e
MustafaKrc <mustafa.kirci0@gmail.com>
2023-12-03 11:29:56 +0300

    Support for Environment variables & small changes
    Sensitive information are now stored in .env file.
    Developer must create .env file in the root directory of this repo.
    Requirements.txt is now in root directory

--- baseball/config.py
+++ baseball/config.py
@@ -1,16 +1,20 @@
-import maskPassword
+from os import getenv
+from dotenv import load_dotenv
 
 class Config:
     # Flask app configuration
     DEBUG = True 
     SECRET_KEY = 'hcq94qhf394f0bu80q82093r'
 
+    load_dotenv() # Load environment variables from .env file
+    #.env file is located in root directory of project
+
     # MySQL database configuration
     MYSQL_CONN = {
-        'host' : 'localhost',
-        'user' : 'root',
-        'password' : maskPassword.maskPsw(),
+        'host' : getenv('DB_HOST', 'localhost'),
+        'user' : getenv('DB_USER', 'root'),
+        'password' : getenv('DB_PASSWORD'),
         'database' : 'lahman_2014'
     } 
-
+    
     PER_PAGE = 16
