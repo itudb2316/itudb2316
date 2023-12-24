2dbe35b38930b2b276e9559592701d69e02839c9
Ömer Yıldırım <yildirimom19@itu.edu.tr>
2023-12-24 21:26:12 +0300

    Back-end models of Teams table which interact with database via SQL queries and commit changes are implemented.
      - Teams table designed and implemented with all CRUD operations.
      - SQL queries of Teams table is designed and implemented.
      - Individually, back-end models which interact with database via SQL queries and commit changes are implemented.
      - models.py file is committed, now Teams table is functional with all CRUD operations.

---
+++ baseball/app/teams/models.py
@@ -0,0 +1,150 @@
+import mysql.connector as dbapi
+from flask import current_app as app
+from app.tools import list2dict
+
+class Teams:
+    HEADER = ['yearID', 'lgID', 'teamID', 'franchID', 'divID', 'teams.Rank',
+              'G', 'Ghome', 'W', 'L', 'DivWin', 'WCWin',
+              'LgWin', 'WSWin', 'R', 'AB', 'H', '2B',
+              '3B', 'HR', 'BB', 'SO', 'SB', 'CS', 'HBP',
+              'SF', 'RA', 'ER', 'ERA', 'CG', 'SHO', 'SV', 'IPouts', 'HA', 'HRA',
+              'BBA', 'SOA', 'E', 'DP', 'FP', 'name', 'park', 'attendance', 'BPF', 'PPF', 'teamIDBR', 'teamIDlahman45', 'teamIDretro']
+    
+
+    COL_TYPES = ['int', 'str', 'str', 'str', 'str', 'int', 'int', 'int', 'int', 'int', 'str', 'str',
+                 'str', 'str', 'int', 'int','int','int','int','int','int','int','int','int','int','int',
+                 'int','int','int','int','int','int','int','int','int','int','int','int','int','int',
+                 'str', 'str', 'int', 'int', 'int', 'str', 'str', 'str']
+
+    def __init__(self):
+        self.app = app
+
+    def view_teams(self, row, sort_by=None, order=None, exclude_null=True): 
+        try:
+            db =  dbapi.connect(**self.app.config['MYSQL_CONN'])
+            cursor = db.cursor()
+            data = list2dict(row, self.HEADER)
+            select_query = 'SELECT '
+            select_query += ", ".join(self.HEADER)
+
+
+            select_query += ' FROM teams WHERE '
+            conditions = []
+            for i in range(len(self.HEADER)):
+                if data[self.HEADER[i]] == 'None':
+                    continue
+                if self.COL_TYPES[i] == 'int':
+                    conditions.append(self.HEADER[i] + ' = ' + data[self.HEADER[i]])
+                elif self.COL_TYPES[i] == 'str':
+                    conditions.append(self.HEADER[i] + ' = \'' + data[self.HEADER[i]] + '\'')
+
+            select_query += " AND ".join(conditions)
+            if len(conditions) == 0:
+                select_query = select_query.removesuffix('WHERE ')
+
+            if sort_by != None:
+                select_query += ' ORDER BY '
+                if exclude_null:
+                    select_query += 'CASE WHEN ' + sort_by + ' IS NULL THEN 1 ELSE 0 END, '
+                select_query += sort_by + ' ' + order
+
+            print()
+            print(select_query)
+            cursor.execute(select_query)
+            results = cursor.fetchall()
+            db.commit()
+
+        except dbapi.Error as err:
+            db.rollback()
+        finally:
+            cursor.close()
+            db.close()
+        return results
+
+    def update_teams(self, transmit, row):
+        try:
+            db =  dbapi.connect(**self.app.config['MYSQL_CONN'])
+            cursor = db.cursor()
+            data = list2dict(row, self.HEADER)
+            update_query = 'UPDATE teams SET '
+            for i in range(len(self.HEADER)):
+                if data[self.HEADER[i]] == 'None':
+                    update_query += self.HEADER[i] + ' = NULL AND '
+                elif self.COL_TYPES[i] == 'int':
+                    update_query += self.HEADER[i] + ' = ' + data[self.HEADER[i]] + ' , '
+                elif self.COL_TYPES[i] == 'str':
+                    update_query += self.HEADER[i] + ' = \'' + data[self.HEADER[i]] + '\' , '
+            update_query = update_query.removesuffix(' , ')
+            update_query += ' WHERE '
+            for i in range(len(self.HEADER)):
+                if transmit[i] == 'None':
+                    update_query += self.HEADER[i] + ' IS NULL AND '
+                elif self.COL_TYPES[i] == 'int':
+                    update_query += self.HEADER[i] + ' = ' + transmit[i] + ' AND '
+                elif self.COL_TYPES[i] == 'str':
+                    update_query += self.HEADER[i] + ' = \'' + transmit[i] + '\' AND '
+            update_query = update_query.removesuffix(' AND ')
+            print()
+            print(update_query)
+            cursor.execute(update_query)
+            db.commit()
+        except dbapi.Error as err:
+            db.rollback()
+            results = []
+        finally:
+            cursor.close()
+            db.close()
+
+    def delete_teams(self, row):
+        try:
+            db =  dbapi.connect(**self.app.config['MYSQL_CONN'])
+            cursor = db.cursor()
+            data = list2dict(row, self.HEADER)
+            delete_query = 'DELETE FROM teams WHERE '
+            for i in range(len(self.HEADER)):
+                if data[self.HEADER[i]] == 'None':
+                    condition = self.HEADER[i] + ' IS NULL AND '
+                elif self.COL_TYPES[i] == 'int':
+                    condition = self.HEADER[i] + ' = ' + data[self.HEADER[i]] + ' AND '
+                elif self.COL_TYPES[i] == 'str':
+                    condition = self.HEADER[i] + ' = \'' + data[self.HEADER[i]] + '\' AND '
+                delete_query += condition
+            delete_query = delete_query.removesuffix(' AND ')
+            print()
+            print(delete_query)
+            cursor.execute(delete_query)
+            db.commit()
+        except dbapi.Error as err:
+            db.rollback()
+        finally:
+            cursor.close()
+            db.close()
+
+    def insert_teams(self, row):
+        try:
+            db =  dbapi.connect(**self.app.config['MYSQL_CONN'])
+            cursor = db.cursor()
+            data = list2dict(row, self.HEADER)
+            insert_query = 'INSERT INTO teams ('
+            for i in self.HEADER:
+                insert_query += i + ', '
+            insert_query = insert_query.removesuffix(', ')
+            insert_query += ') VALUES ('
+            for i in range(len(self.HEADER)):
+                if data[self.HEADER[i]] == 'None':
+                    insert_query += 'NULL, '
+                elif self.COL_TYPES[i] == 'int':
+                    insert_query +=  data[self.HEADER[i]] + ', '
+                elif self.COL_TYPES[i] == 'str':
+                    insert_query += '\'' + data[self.HEADER[i]] + '\', '
+            insert_query = insert_query.removesuffix(', ')
+            insert_query += ')'
+            print()
+            print(insert_query)
+            cursor.execute(insert_query)
+            db.commit()
+        except dbapi.Error as err:
+            db.rollback()
+        finally:
+            cursor.close()
+            db.close()
